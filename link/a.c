04f279129f5702a5f2773d9e6ad1b58a82aefabc
刘帅 <liu.shuai@hejinonline.com>
2018-06-27 09:39:39 +0800

    new file:   hotspot/Oracle JRockit_The Definitive Guide.pdf new file:   hotspot/interpreter/frame.org modified:   hotspot/interpreter/interpreter.org new file:   hotspot/interpreter/javaControlStack.org renamed:    hotspot/interpreter/generate_normal_entry.org -> hotspot/interpreter/method_entry.org renamed:    hotspot/interpreter/generate_return_method_entry.org -> hotspot/interpreter/return_method_entry.org new file:   hotspot/interpreter/templateTable.org modified:   link/a.c modified:   link/a.o modified:   link/b.c modified:   link/b.o modified:   面试.org

--- link/a.c
+++ link/a.c
@@ -1,8 +1,8 @@
 extern int shared;
 
-void swap(int* a, int* b);
+void swap(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k);
 
 int main() {
     int a = 100;
-    swap(&a, &shared);
+    swap(a, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 }
--- link/b.c
+++ link/b.c
@@ -1,5 +1,10 @@
 int shared = 1;
 
-void swap(int* a, int* b) {
-    *a ^= *b ^= *a ^= *b;
+void swap(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k) {
+    	int temp = a;
+	a = b;
+	b = temp;
+	k++;
+	j++;
+	i++;
 }
